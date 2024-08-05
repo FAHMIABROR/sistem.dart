43d48833cd2339dfc0435542e369f996eeb0a098
abror <fahmiabroridnsolo@gmail.com>
2024-08-05 08:10:36 +0700

    first commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,13 @@
+void main() {
+  String validName = 'aboy';
+  String validPassword = 'botak';
+
+  String inputName = 'aboy';
+  String inputPassword = 'botak';
+
+  if (validName == inputName && validPassword == inputPassword) {
+    print('selamattt anda telahh masuk');
+  } else {
+    print('mungkin ada yang salah dengan user name an pssword');
+  }
+}
